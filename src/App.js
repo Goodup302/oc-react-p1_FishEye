02afb2d7408df4d3c1db20749232904d714f60e3
Julien Fernandes <j.f0471430704@gmail.com>
2021-10-01 11:59:47 +0200

    add js libs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import "./assets/scss/app.scss"
+import fishEyeData from "./FishEyeData.json"
+
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    console.log(fishEyeData);
+    return (
+        <div className="App">
+            <div className="row">
+                <div className="col-6">
+                    <h1>test</h1>
+                </div>
+                <div className="col-6">
+                    <h1>test</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+//serviceWorker
+window.onload = () => {
+    if('serviceWorker' in  navigator) {
+        navigator.serviceWorker.register('/serviceWorker.js')
+            .then(function(reg) {
+                console.log('serviceWorker Registration succeeded');
+            }).catch(function(err) {
+            console.error('Registration failed with ' + err);
+        })
+    }
+}
